ae020aed3cacfea86de7db1372fdc1b6ac96c639
Felipe <felipe__andrew@hotmail.com>
2022-08-12 16:35:49 -0300

    fix lint

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,20 +2,20 @@
 
 const fetchProducts = async (query) => {
   // seu código aqui
-  try{
+  try {
     const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-    const res = await fetch(url)
-    const data = await res.json()
+    const res = await fetch(url);
+    const data = await res.json();
       // .then((data) => data.results[0].thumbnail)
       // return res.results[0].title
       return data;
       
       // console.log(results);
-  } catch (error){
+  } catch (error) {
     return error;
   }
 
-  };
+};
 
 if (typeof module !== 'undefined') {
   module.exports = {
